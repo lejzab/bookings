77cf041879284ddc7a7daa8690910eba28f45eac
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2021-06-14 16:19:52 +0200

    all html files recreated as templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -46,3 +46,28 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+//Reservation renders the reservation page
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+//Majors is majors suite page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+//Generals renders general's quarter page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+//Contact renderd contact pahe
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
+
+// Availability checks availability of rooms
+func (m *Repository) Availability(w http.ResponseWriter, request *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
